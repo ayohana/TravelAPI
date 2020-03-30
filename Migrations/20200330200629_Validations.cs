f4666d671077de5b6258285828c1ed161f8c29bf
Adela Darmansyah <adela.yohana@gmail.com>
2020-03-30 13:08:40 -0700

    Add validations using data annotations

---
+++ Migrations/20200330200629_Validations.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace TravelAPI.Migrations
+{
+    public partial class Validations : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Title",
+                table: "Reviews",
+                maxLength: 30,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Destination",
+                table: "Reviews",
+                maxLength: 15,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Description",
+                table: "Reviews",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldNullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Title",
+                table: "Reviews",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldMaxLength: 30);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Destination",
+                table: "Reviews",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldMaxLength: 15);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Description",
+                table: "Reviews",
+                nullable: true,
+                oldClrType: typeof(string));
+        }
+    }
+}
--- Models/Review.cs
+++ Models/Review.cs
@@ -1,11 +1,26 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace TravelAPI.Models
 {
     public class Review
     {
+        [Key]
         public int ReviewId { get; set; }
+
+        [Required]
+        [StringLength(15, ErrorMessage="Destination name cannot exceed 15 characters")]
         public string Destination { get; set; }
+       
+        [Required]
+        [MaxLength(30, ErrorMessage="Title cannot exceed 30 characters.")]
         public string Title { get; set; }
+       
+        [Required]
+        [MinLength(5, ErrorMessage="Description must be at least 5 characters.")]
         public string Description { get; set; }
+      
+        [Required]
+        [Range(1, 5, ErrorMessage="Rating must be between 1 and 5.")]
         public int Rating { get; set; }
     }
 }
